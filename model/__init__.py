1cfa5f34c431b27111d7368b11f3177a55253b93
Tasos <anastasios.stamoulakatos@strath.ac.uk>
2022-10-16 11:53:34 +0100

    generate model

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+from .modules import *
+from .ibn_3d_net import *
+from .resnet_ibn import *
+from .resnet3d import *
+from .utils import *
+
---
+++ model/utils.py
@@ -0,0 +1,34 @@
+import pickle
+import sys
+from .ibn_3d_net import *
+from .modules import *
+from .resnet3d import *
+import math
+from functools import partial
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import matplotlib.pyplot as plts
+import numpy as np
+from torch.autograd import Variable
+from pathlib import Path
+from PIL import ImageFilter
+import numpy
+from torchvision import transforms
+import re
+from PIL import Image
+import sys
+from collections import namedtuple
+import glob
+from tqdm import trange
+import time
+
+
